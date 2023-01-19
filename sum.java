79a6e83edad5926d81f1ed16050cdea60fd951d0
Bharat Sheoran <bharatsheoran03@gmail.com>
2023-01-19 18:50:35 +0530

    adding sum.java

---
+++ sum.java
@@ -0,0 +1,10 @@
+package LetsLearnGithub;
+
+public class sum {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+
+        System.out.println("The sum is " + (a + b));
+    }
+}
