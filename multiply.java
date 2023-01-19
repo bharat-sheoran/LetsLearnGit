6abab7fdf4af0f26122b11e318321233c612b9fb
Bharat Sheoran <bharatsheoran03@gmail.com>
2023-01-19 19:14:26 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,9 @@
+package LetsLearnGithub;
+
+public class multiply {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+        System.out.println("The multiplication is: " + (a * b));
+    }
+}
